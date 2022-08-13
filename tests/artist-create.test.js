e6fa1ec64147125f86fccd9bb227fdb590350267
Agnes Beviz <agnes.beviz@gmail.com>
2022-08-13 12:32:17 +0100

    mocha first test passing

--- tests/artist-create.test.js
+++ tests/artist-create.test.js
@@ -1,18 +1,18 @@
-const { expect } = require('chair');
+const { expect } = require('chai');
 const request = require('supertest');
 const app = require('../src/app');
 
 describe('create artist', () => {
-    describe ('/artist', () => {
+    describe('/artist', () => {
         describe('POST', () => {
             it('creates a new artist in the database', async () => {
-                const res = await (await request(app).post('/artist')).send({
-                    name:  'Tama Impala',
+                const res = await request(app).post('/artist').send({
+                    name: 'Tame Impala',
                     genre: 'rock',
                 });
 
                 expect(res.status).to.equal(201);
-            })
-        })
-    })
-})
+            });
+        });
+    });
+});
